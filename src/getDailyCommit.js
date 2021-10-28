aebd8d386cf4d96544cdef0665ea644fdf5e9e44
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-10-28 14:58:38 +0900

    develop

--- src/getDailyCommit.js
+++ src/getDailyCommit.js
@@ -1,7 +1,7 @@
 const { graphqlWithAuth } = require('./graphql.js');
 
 const QUERY = `
-  {
+  query($user: String!) {
     user(login: $user) {
       contributionsCollection(from: "2021-10-25T00:00:00", to: "2021-10-26T00:00:00") {
         totalCommitContributions
@@ -11,7 +11,7 @@ const QUERY = `
 
 exports.getDailyCommit = async() => {
   try {
-    const res = await graphqlWithAuth(QUERY, {user: 'nacal'});
+    const res = await graphqlWithAuth(QUERY, {user: `${process.env.USERNAME}`});
     return res.user.contributionsCollection.totalCommitContributions;
   } catch (err) {
     console.error(err.message);
