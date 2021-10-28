7ce390f8099e167fbbd0cf8939aaa100ff377e66
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-10-28 15:24:55 +0900

    develop

--- src/getDailyCommit.js
+++ src/getDailyCommit.js
@@ -2,7 +2,7 @@ const { graphqlWithAuth } = require('./graphql.js');
 const { getDate } = require('./getDate.js');
 
 const QUERY = `
-  query($user: String!, $from: String!, $to: String!) {
+  query($user: String!, $from: DateTime, $to: DateTime) {
     user(login: $user) {
       contributionsCollection(from: $from, to: $to) {
         totalCommitContributions
