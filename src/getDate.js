6432f5e8b1ce3a52bad33f4edad51f74c3f6e6fd
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-11-01 00:18:26 +0900

    fix

--- src/getDate.js
+++ src/getDate.js
@@ -1,6 +1,5 @@
 exports.getDate = () => {
   const date = new Date();
-  date.setDate(date.getDate() - 1);
 
   const year    = date.getFullYear(),
         month   = date.getMonth()+ 1,
@@ -8,8 +7,6 @@ exports.getDate = () => {
         formatDate = `${year}-${month}-${day}`,
         from = `${formatDate}T00:00:00`,
         to = `${formatDate}T23:59:59`;
-
-  console.log(from, to)
 
   return {from: from, to: to}
 }
