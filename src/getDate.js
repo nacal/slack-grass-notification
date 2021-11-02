b63ddf9cb68a7d0216bf6adcddb2639880ac4690
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-11-02 09:48:14 +0900

    fix day format

--- src/getDate.js
+++ src/getDate.js
@@ -1,12 +1,16 @@
-exports.getDate = () => {
+const getDate = () => {
   const date = new Date();
 
   const year    = date.getFullYear(),
         month   = date.getMonth()+ 1,
-        day    = date.getDate(),
+        day    = ("0"+date.getDate()).slice(-2),
         formatDate = `${year}-${month}-${day}`,
         from = `${formatDate}T00:00:00`,
         to = `${formatDate}T23:59:59`;
+  console.log(from, to)
 
   return {from: from, to: to}
 }
+
+
+getDate();
