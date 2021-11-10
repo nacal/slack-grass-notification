33f6f1892350ba79b0fadaec97fa0d1866bc4ec0
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-11-10 14:56:24 +0900

    fix month format

--- src/getDate.js
+++ src/getDate.js
@@ -2,8 +2,8 @@ exports.getDate = () => {
   const date = new Date();
 
   const year    = date.getFullYear(),
-        month   = date.getMonth()+ 1,
-        day    = ("0"+date.getDate()).slice(-2),
+        month   = ("0" + (date.getMonth() + 1)).slice(-2),
+        day    = ("0" + date.getDate()).slice(-2),
         formatDate = `${year}-${month}-${day}`,
         from = `${formatDate}T00:00:00`,
         to = `${formatDate}T23:59:59`;
